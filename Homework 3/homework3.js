c18f5d4da219fab3a54782f97a62edbae343ce73
MovsisyanMher <mher.movsisyan2011@yandex.ru>
2018-09-19 14:22:25 -0700

    DevPush 5

--- Homework 3/homework3.js
+++ Homework 3/homework3.js
@@ -1,31 +1,53 @@
-//#region Homework 2
+//#region Homework 3
+
+
+/* A function that takes at least three arguments and returns the result of some set of operations using those arguments - 5 points */
+const beep = function(arg0, arg1, arg2){ return arg1 + (arg2 + arg0)};
 
 
 
-// At least two constants
-const beep = true;
-const boop = false;
+/* A function that takes no arguments and returns something - 5 points */
+const pi = () => {return 3.14159265358979323846264338327950288;};
 
-// At least two arithmetic operators
-const badabeep = 1 + 2;
-const badaboop = 5 * 3 - 1;
 
-// At least one integer and one string
-const badoop = "Alexa play despacito";
-const badaap = 42069;
 
-// At least one if/else block
-if(beep === boop)
+/* A function that takes arguments, does something but does not return anything - 6 points */
+const System = {Console: {WriteLine: function(arg){console.log(arg);}}};
+System.Console.WriteLine("hehe this works now, hurray c# users!");
+
+
+/* A function that takes three strings and returns the string that is the longest.  - 6 points */
+const longlegs = function(str0, str1, str2)
 {
-    console.log("This is so sad, " + badoop);
-}
-else 
-{
-    console.log("Thank you Kanye, very cool");
+    const card0 = str0.toString().length;
+    const card1 = str1.toString().length;
+    const card2 = str2.toString().length;
+
+    if(card0 > card1 && card0 > card2){return card0;}
+    else if(card1 > card0 && card1 > card2){return card1;}
+    else if(card1 > card0 && card1 > card2){return card1;}
+    else 
+    {
+        System.Console.WriteLine("These arguments cannot compete!");
+        return "These arguments cannot compete!";
+    };
 };
 
-//Print at least one thing to console
-console.log("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+
+/* A function that takes three strings and returns the string that is the longest.  - 6 points */
+const compareNum = function(num1, num2)
+{
+    if(num1 === undefined || num2 === undefined){return null;}
+    else if(num1 < num2){return -1;}
+    else if(num1 > num2){return 1;}
+    else if(num1 === num2){return 0;};
+};
+
+
+
+/* A function that takes three strings and returns the string that is the longest.  - 6 points */
+
 
 
 //#endregion

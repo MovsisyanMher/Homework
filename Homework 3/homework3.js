a69851711ead0f59ce15d6a7ae663e86a10630d4
MovsisyanMher <mher.movsisyan2011@yandex.ru>
2018-09-19 09:11:11 -0700

    OrganizePush 1

---
+++ Homework 3/homework3.js
@@ -0,0 +1,31 @@
+//#region Homework 2
+
+
+
+// At least two constants
+const beep = true;
+const boop = false;
+
+// At least two arithmetic operators
+const badabeep = 1 + 2;
+const badaboop = 5 * 3 - 1;
+
+// At least one integer and one string
+const badoop = "Alexa play despacito";
+const badaap = 42069;
+
+// At least one if/else block
+if(beep === boop)
+{
+    console.log("This is so sad, " + badoop);
+}
+else 
+{
+    console.log("Thank you Kanye, very cool");
+};
+
+//Print at least one thing to console
+console.log("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+
+//#endregion

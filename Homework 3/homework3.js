36efd7bf296e58524a50d0a121d73176404128a9
MovsisyanMher <mher.movsisyan2011@yandex.ru>
2018-09-20 10:13:31 -0700

    DevPush 6

--- Homework 3/homework3.js
+++ Homework 3/homework3.js
@@ -16,38 +16,83 @@ const System = {Console: {WriteLine: function(arg){console.log(arg);}}};
 System.Console.WriteLine("hehe this works now, hurray c# users!");
 
 
-/* A function that takes three strings and returns the string that is the longest.  - 6 points */
+/* A function that takes three strings and returns the string that is the longest. - 6 points */
 const longlegs = function(str0, str1, str2)
 {
-    const card0 = str0.toString().length;
-    const card1 = str1.toString().length;
-    const card2 = str2.toString().length;
-
-    if(card0 > card1 && card0 > card2){return card0;}
-    else if(card1 > card0 && card1 > card2){return card1;}
-    else if(card1 > card0 && card1 > card2){return card1;}
-    else 
-    {
-        System.Console.WriteLine("These arguments cannot compete!");
-        return "These arguments cannot compete!";
-    };
+	// Store the lengths of the strings
+	const card1 = str1.toString().length;
+	const card0 = str0.toString().length;
+	const card2 = str2.toString().length;
+	
+	// Compare the strings
+	if(card0 > card1 && card0 > card2){return card0;}
+	else if(card1 > card0 && card1 > card2){return card1;}
+	else if(card2 > card0 && card2 > card1){return card2;}
+	else 
+	{
+		System.Console.WriteLine("These arguments cannot compete!");
+		return "These arguments cannot compete!";4
+	};
 };
 
 
 
-/* A function that takes three strings and returns the string that is the longest.  - 6 points */
+/* A function that takes two numbers and returns 0 if they are equal, 1 if 1st > 2nd and -1 if 1st < 2nd  - 6 points */
 const compareNum = function(num1, num2)
 {
-    if(num1 === undefined || num2 === undefined){return null;}
-    else if(num1 < num2){return -1;}
-    else if(num1 > num2){return 1;}
-    else if(num1 === num2){return 0;};
+   if(num1 === undefined || num2 === undefined || num1 === null || num2 === null){return null;}
+   else if(num1 < num2){return -1;}
+   else if(num1 > num2){return 1;}
+	else if(num1 === num2){return 0;}
+	else {return null;};
 };
 
 
 
-/* A function that takes three strings and returns the string that is the longest.  - 6 points */
+/* Create a multiply function - 2 points */
+const mult = function(num1, num2)
+{ 
+	if(num1 === undefined || num2 === undefined || num1 === null || num2 === null){return null;};
+	return (num1 * num2);
+	
+};
 
+/* Create a divide function - 2 points */
+const div = function(num1, num2)
+{ 
+	if(num1 === undefined || num2 === undefined || num1 === null || num2 === null || num2 == 0 /*The == is on purpose*/ ){return null;};
+	return (num1 / num2);
+	
+};
+
+/* Create a function that calculates the area of a triangle without using * or / - 6 points */
+const triangleArea = function(base, height){ return div(mult(base, height), 2);};
+
+/* Create a function that returns the number of digits - 6 points */
+const numLength = function(num){ return num.toString().length;};
+
+/* Create a function that takes two strings and one number, if the (concat of the strings).length > num return 1 else -1 and 0 for === - 6 points */
+const sizer = function(str0, str1, num)
+{ 
+	if (num === undefined || num === null){return null;}
+	else if ((str0 + str1).length > num){return 1;}
+	else if ((str0 + str1).length < num){return -1;}
+	else if ((str0 + str1).length === num){return 0;}
+	else 
+	{
+		System.Console.WriteLine("Bad input");
+		return null;
+	};
+};
+
+
+/* Create a function that takes two strings and one number, if the (concat of the strings).length > num return 1 else -1 and 0 for === - 6 points */
+const runStuff = function(num0, num1, str)
+{
+	if (str === "rectangle"){return (num0 * num1);}
+	else if (str === "triangle"){return ((num0 * num1)/2);}
+	else {return null;};
+};
 
 
 //#endregion
